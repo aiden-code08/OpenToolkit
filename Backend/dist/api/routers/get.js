32b7edb8dba04e57385c41b646314f463b70abcd
aiden-code08 <aidenmohr@icloud.com>
2025-12-07 20:43:52 -0500

    added: get scenes

--- Backend/dist/api/routers/get.js
+++ Backend/dist/api/routers/get.js
@@ -13,7 +13,15 @@ exports.getRouter.get("/bitrate/:sessionid", (req, res) => {
         });
     res.status(200).json({ bitrate: session.bitrate.getAvg(sessionId) });
 });
-exports.getRouter.get("/scenes/:sessionid", (req, res) => {
+exports.getRouter.get("/scenes/:sessionid", async (req, res) => {
     const sessionId = req.params.sessionid;
     const session = OpenToolkit_1.sessions.get(sessionId);
+    if (!session)
+        return res.status(404).json({
+            message: "Session not found",
+            code: 6
+        });
+    const scene_list = await session.obs.obs.call("GetSceneList");
+    console.log(scene_list);
+    return res.status(200).json(scene_list);
 });
